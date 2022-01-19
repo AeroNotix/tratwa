3333226b29874aa3471d6b72f05a885f1e6c1330
Aaron France <aaron.l.france@gmail.com>
2022-01-19 20:29:40 +0100

    implement rpc module

--- lib/rpc.ml
+++ lib/rpc.ml
@@ -1,1 +1,19 @@
-type t = int
+type request_vote = {
+  term: int;
+  candidate_id: Candidate.t;
+  last_log_index: int;
+  last_log_term: int;
+}
+
+type append_entries = {
+  term: int;
+  leader_id: int;
+  prev_log_index: int;
+  prev_log_term: int;
+  entries: Logentry.t list;
+  leader_commit: int
+}
+
+type t =
+  | RequestVote of request_vote
+  | AppendEntries of append_entries
